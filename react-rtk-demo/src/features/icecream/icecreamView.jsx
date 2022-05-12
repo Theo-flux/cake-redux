3e771e694aec14874215c79b92b744748a1b153b
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-12 18:42:54 +0100

    icecreamView

--- react-rtk-demo/src/features/icecream/icecreamView.jsx
+++ react-rtk-demo/src/features/icecream/icecreamView.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function icecreamView() {
   return (
     <div>
-        <h2>Number of icecreams: </h2>
-        <button>order</button>
-        <button>restock</button>
+        <h2>Number of ice creams - </h2>
+        <button>order ice cream</button>
+        <button>restock ice cream</button>
     </div>
   )
 }
